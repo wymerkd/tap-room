f825d24b8a28e720b2c671bba33aa2179cda2541
wymerkd <wymerkd@gmail.com>
2020-02-14 11:14:14 -0800

    add style to numer and name props

--- src/components/Beer.jsx
+++ src/components/Beer.jsx
@@ -3,16 +3,36 @@ import PropTypes from 'prop-types';
 
 
 function Beer(props){
+  const numberStyle = {
+    fontSize: '20px',
+    fontWeight: 'bold',
+    borderStyle: 'none',
+    borderRadius: '50%',
+    background: '#808080',
+    background: '-webkit-linear-gradient(to right, #808080, #3fada8)',
+    background: 'linear-gradient(to right, #808080, #3fada8)',
+    padding: "4%"
+  }
+
+  const nameStyle = {
+    fontSize: '24px',
+    textTransform: 'uppercase',
+    wordSpacing: '3px',
+    paddingRight: '5px'
+  }
+
 return (
   <div>
-    <span>{props.number}</span>
-    <span>{props.name}</span>
-    <span>{props.brewer}</span>
-    <span>{props.beerType}</span>
-    <span>{props.location}</span>
-    <span>{props.percentage}</span>
-    <span>{props.ounces}</span>
-    <span>{props.price}</span>
+    <div>
+      <span style={numberStyle}>{props.number}</span>
+      <span style={nameStyle}>{props.name}</span>
+      <span>{props.brewer}</span>
+      <span>{props.beerType}</span>
+      <span>{props.location}</span>
+      <span>{props.percentage}</span>
+      <span>{props.ounces}</span>
+      <span>{props.price}</span>
+    </div>
   </div>
   );
 }
